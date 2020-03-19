97b22f20b4e929207cc5b60a0afed911e94349e8
DaniilPalii <palii.daniil.o@gmail.com>
2020-03-19 01:32:58 +0100

    Update Syncfusion to stable licensed version - fix template errors

--- ExchangeAdvisor.SignalRClient/Startup.cs
+++ ExchangeAdvisor.SignalRClient/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Syncfusion.EJ2.Blazor;
+using Syncfusion.Licensing;
 
 namespace ExchangeAdvisor.SignalRClient
 {
@@ -23,6 +25,8 @@ namespace ExchangeAdvisor.SignalRClient
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSyncfusionBlazor();
+
             services.AddHttpClient();
             services.AddScoped<IRateWebFetcher, RateWebFetcher>();
             services.AddScoped<IRateForecaster, RateForecaster>();
@@ -32,6 +36,8 @@ namespace ExchangeAdvisor.SignalRClient
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            SyncfusionLicenseProvider.RegisterLicense("MjI0NjcwQDMxMzcyZTM0MmUzMG51eUxNa3JXYWJHb0dkYXVvU3pXNTRVbENkNGVBc2FGNERDa1Y4LzkyZ0E9"); // TODO: provide key with configuration
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
